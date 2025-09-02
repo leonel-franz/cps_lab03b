a5d776226fceddf9ec5e456e3bc8806b4fccdfb8
Leonel <leonel.portillo@tecsup.edu.pe>
2025-09-01 20:33:42 -0500

    Modificacion de StudentController

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -5,5 +5,10 @@ public class StudentController {
     public static void main(String[] args) {
         System.out.println("Hola desde StudentController!");
     }
+
+    public void greet() {
+        System.out.println("Welcome to Sprint-2! (Responsable)");
+    }
+
 }
 
